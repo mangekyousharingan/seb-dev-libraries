254a1b7ee3d75b946cdc45df927ad50f2e082aad
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2023-07-26 21:53:54 +0200

    setup py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="seb-dev-libraries",
+    version="0.0.1",
+    author="Seb",
+    author_email="wdowiarzsebastian@gmail.com",
+    packages=["seb-dev-libraries"],
+    description="Package hold development dependencies.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mangekyousharingan/seb-dev-libraries",
+    license='MIT',
+    python_requires='>=3.11',
+    install_requires=[]
+)
